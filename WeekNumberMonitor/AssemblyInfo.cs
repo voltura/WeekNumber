1cb22d824afde6f550d73d31adad72d871db3c27
Joakim Skoglund <joakim@voltura.se>
2021-05-28 23:16:06 +0200

    Monitor to clean taskbar icon if WeekNumber.exe terminated

---
+++ WeekNumberMonitor/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeekNumberMonitor")]
+[assembly: AssemblyDescription("WeekNumberMonitor")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Voltura AB")]
+[assembly: AssemblyProduct("WeekNumberMonitor")]
+[assembly: AssemblyCopyright("Copyright © Voltura AB 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("65632C08-E70E-4C7F-94F2-BC68FBC42744")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
---
+++ WeekNumberMonitor/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        int _id = int.Parse(args[0]);
+        GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+        GCSettings.LatencyMode = GCLatencyMode.Batch;
+        try
+        {
+            using (Process p = Process.GetProcessById(_id)) 
+            {
+                p.WaitForExit();
+            }
+        }
+        catch (Exception)
+        {
+        }
+        RefreshTrayArea();
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass,
+        string lpszWindow);
+
+    [DllImport("user32.dll")]
+    private static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr SendMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+    private static void RefreshTrayArea()
+    {
+        IntPtr systemTrayContainerHandle = FindWindow("Shell_TrayWnd", null);
+        IntPtr systemTrayHandle = FindWindowEx(systemTrayContainerHandle, IntPtr.Zero, "TrayNotifyWnd", null);
+        IntPtr sysPagerHandle = FindWindowEx(systemTrayHandle, IntPtr.Zero, "SysPager", null);
+        IntPtr notificationAreaHandle = FindWindowEx(sysPagerHandle, IntPtr.Zero, "ToolbarWindow32", "Notification Area");
+        if (notificationAreaHandle == IntPtr.Zero)
+        {
+            notificationAreaHandle = FindWindowEx(sysPagerHandle, IntPtr.Zero, "ToolbarWindow32",
+                "User Promoted Notification Area");
+            IntPtr notifyIconOverflowWindowHandle = FindWindow("NotifyIconOverflowWindow", null);
+            IntPtr overflowNotificationAreaHandle = FindWindowEx(notifyIconOverflowWindowHandle, IntPtr.Zero,
+                "ToolbarWindow32", "Overflow Notification Area");
+            RefreshTrayArea(overflowNotificationAreaHandle);
+        }
+        RefreshTrayArea(notificationAreaHandle);
+    }
+
+    private static void RefreshTrayArea(IntPtr windowHandle)
+    {
+        const uint wmMousemove = 0x0200;
+        GetClientRect(windowHandle, out RECT rect);
+        for (var x = 0; x < rect.right; x += 5)
+            for (var y = 0; y < rect.bottom; y += 5)
+                SendMessage(windowHandle, wmMousemove, 0, (y << 16) + x);
+    }
+
+}
